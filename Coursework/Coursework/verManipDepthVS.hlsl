5beaac764cb7d05ac587f3502fcea6a2a8c0d259
HamishHill-WK <57226991+HamishHill-WK@users.noreply.github.com>
2022-04-03 14:13:29 +0100

    removed unused variables from depth shader matrix buffer

--- Coursework/Coursework/verManipDepthVS.hlsl
+++ Coursework/Coursework/verManipDepthVS.hlsl
@@ -6,8 +6,7 @@ cbuffer MatrixBuffer : register(b0)
     matrix worldMatrix;
     matrix viewMatrix;
     matrix projectionMatrix;
-    matrix lightViewMatrix;
-    matrix lightProjectionMatrix;
+
 };
 
 struct InputType
